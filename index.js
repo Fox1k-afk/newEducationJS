9cb1d0088964ee29defe9c4abe1006208d058a1d
Vladyslav <Onoprienko.77@gmail.com>
2022-06-16 16:41:52 +0300

    workDone

--- index.js
+++ index.js
@@ -1,132 +1,35 @@
-const ACTIONS = { '+': add, '-': sub, '*': mul, '/': div };
-const ACTION_LIST = Object.keys(ACTIONS);
-
-main();
-function main() {
-	const action = getAction();
-	const operandsCount = getOperandsCount();
-	const numbers = getNumbers(operandsCount);
-	const result = calc(numbers, action);
-	showResult(numbers, action, result);
+const students = [
+	{
+		id: 10,
+		name: 'John Smith',
+		marks: [10, 8, 6, 9, 8, 7],
+	},
+	{
+		id: 11,
+		name: 'John Doe',
+		marks: [9, 8, 7, 6, 7],
+	},
+	{
+		id: 12,
+		name: 'Thomas Anderson',
+		marks: [6, 7, 10, 8],
+	},
+	{
+		id: 13,
+		name: 'Jean-Baptiste Emanuel Zorg',
+		marks: [10, 9, 8, 9],
+	},
+];
+function avgStudentMark(id) {
+	const { marks } = students.find((st) => st.id == id);
+	return marks.reduce((acc, m) => acc + m) / marks.length;
 }
+console.log(avgStudentMark(10));
 
-function getAction() {
-	let res;
-	do {
-		res = prompt(`Enter operator ${ACTION_LIST.join(', ')}`, '+');
-	} while (!isValidAction(res));
-	return res;
-}
-
-function isValidAction(action) {
-	return ACTION_LIST.includes(action);
-}
-
-function getOperandsCount() {
-	let operandsCount;
-	do {
-		operandsCount = prompt('Enter operands count', 2);
-	} while (
-		!isOperandsCountValid(operandsCount) ||
-		operandsCount < 2 ||
-		operandsCount > 5
+function avgGroupMark(students) {
+	return (
+		students.reduce((acc, { id }) => acc + avgStudentMark(id), 0) /
+		students.length
 	);
-	return Number(operandsCount);
 }
-
-function isOperandsCountValid(operand) {
-	return !isNaN(operand);
-}
-
-function getNumbers(operandsCount) {
-	const numbers = [];
-	for (let i = 1; i <= operandsCount; i++) {
-		numbers.push(getOperand(i));
-	}
-
-	return numbers;
-}
-function getOperand(operandName) {
-	let res;
-	do {
-		res = prompt(`Enter operand ${operandName}`);
-	} while (!isOperandValid(res));
-	return Number(res);
-}
-
-function isOperandValid(operand) {
-	return !isNaN(operand);
-}
-
-function calc(numbers, action) {
-	let res = numbers[0];
-
-	for (let i = 1; i < numbers.length; i++) {
-		res = ACTIONS[action](res, numbers[i]);
-	}
-	return res;
-}
-
-function add(a, b) {
-	return a + b;
-}
-function sub(a, b) {
-	return a - b;
-}
-function mul(a, b) {
-	return a * b;
-}
-function div(a, b) {
-	return a / b;
-}
-
-function showResult(numbers, action, result) {
-	console.log(`${numbers.join(` ${action} `)} = ${result}`);
-}
-///////////////////////// second variation, probably bad ;(
-// let operators = ['+', '-', '*', '/'],
-// 	operation = getOperation(),
-// 	question,
-// 	count = 0,
-// 	operand,
-// 	total = 0;
-
-// function getOperation(operation) {
-// 	while (!operators.includes(operation)) {
-// 		return prompt(`Choose operation ${operators.join(', ')}`, '+');
-// 	}
-// }
-// do {
-// 	question = prompt(
-// 		`How many operands do you want to use? (More than 1 less than 5)`,
-// 		2
-// 	);
-// 	count = +question;
-// } while (count !== parseInt(question) || count < 2 || count > 4);
-
-// let x = [];
-// for (let i = 1; i <= count; i++) {
-// 	do {
-// 		question = prompt(`Enter ${i}-th number`);
-// 		operand = +question;
-// 	} while (operand !== parseFloat(question) || !Number.isFinite(operand));
-// 	x.push(operand);
-// }
-// total = x[0];
-// for (let i = 1; i < count; i++) {
-// 	switch (operation) {
-// 		case '+':
-// 			total += x[i];
-// 			break;
-// 		case '-':
-// 			total -= x[i];
-// 			break;
-// 		case '*':
-// 			total *= x[i];
-// 			break;
-// 		case '/':
-// 			total /= x[i];
-// 			break;
-// 	}
-// }
-// console.log(`${x.join(` ${operation} `)} = ${total}`);
+console.log(avgGroupMark(students));
