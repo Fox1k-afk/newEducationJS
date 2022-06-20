0127784566594c2c9238feca1111e20bc4ce62a8
Vladyslav <Onoprienko.77@gmail.com>
2022-06-20 14:41:49 +0300

    factor

--- index.js
+++ index.js
@@ -69,3 +69,4 @@ console.log(max([8, 17]), '2 elements test, must return 17');
 // console.log(max2([8]), 'one element test, must return 8');
 // console.log(max2([1, 8, 37, 5, 17]), '5 elements test, must return 37');
 // console.log(max2([8, 17]), '2 elements test, must return 17');
+//  whats happened
