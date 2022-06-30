cfcf0c61b02d3aab95db58322f268357282c9925
Vladyslav <Onoprienko.77@gmail.com>
2022-06-30 18:16:53 +0300

    homeWorkDone

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-/// DOM <<<<<
+const ul = document.querySelector('ul');
+const input = document.querySelector('#inputDoing');
+const button = document.querySelector('#btn');
 
-// document.querySelector('.class/id');
-// document.querySelectorAll('.class/id/');
-
-// .value
-// .className
-// .classList
-// .dataset
-// .style
-// .attributes
-// .setAttribute()
-
-// const liEl = document.createElement('li'); // создаем элем
-
-// liEl.textContent = ' 7 elem'; // < задаем текст
-// // liEl.innerText = '7 elem'; // < не юзаем
-
-// ul.append(liEl); // добавляет в конец
-// ul.prepend(liEl); // добавляет в начало
-
-// // liEl.remove();  < удаление элема
-// // liEl = null;    < связка  с удалением
-
-// button.addEventListener('click', onButtonClick);
-// function onButtonClick() {
-// 	document.querySelector('#btn').textContent = 'Clicked';
-// }
+button.addEventListener('click', onButtonClick);
+function onButtonClick() {
+	if (input.value !== '') {
+		const liEl = document.createElement('li');
+		ul.append(liEl);
+		liEl.textContent = input.value;
+		input.value = '';
+	}
+}
